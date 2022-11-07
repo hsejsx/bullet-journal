f613732c29c39ed01d7995059ee61ebf53e51f75
hsejsx <hsejsx@gmail.com>
2022-11-07 21:52:47 +0900

    프로젝트 초기화

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
